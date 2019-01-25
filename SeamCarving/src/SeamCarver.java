3ca5ad90c927014e00e30ed4dabf1c23257d4d62
s-wonget <s-wonget@51L104-14.it.bsd405.org>
2019-01-25 09:09:56 -0800

    CP 2 first attempt: coded up energy

--- SeamCarving/src/SeamCarver.java
+++ SeamCarving/src/SeamCarver.java
@@ -1,8 +1,13 @@
+import java.awt.Color;
+
 public class SeamCarver{
 	private SmC_Picture p;
 	
 	public SeamCarver(SmC_Picture pictureP){
-		p = pictureP;
+		if(pictureP == null){
+			throw new NullPointerException();
+		}
+		p = new SmC_Picture(pictureP);
 	}
 
 	public SmC_Picture picture(){
@@ -18,7 +23,18 @@ public class SeamCarver{
 	}
 
 	public double energy(int x, int y){
-		throw new UnsupportedOperationException();
+		if(x == 0 || y == 0 || x == width() - 1 || y == height() - 1){
+			return 1000.0;
+		}
+		return Math.sqrt(gradientSq(x,y,1,0) + gradientSq(x,y,0,1));
+	}
+	private double gradientSq(int x, int y, int xChange, int yChange){
+		Color left = p.get(x - xChange, y - yChange);
+		Color right = p.get(x + xChange, y + yChange);
+		int r = right.getRed() - left.getRed();
+		int g = right.getGreen() - left.getGreen();
+		int b = right.getBlue() - left.getBlue();
+		return r*r + g*g + b*b;
 	}
 
 	public int[] findHorizontalSeam(){
