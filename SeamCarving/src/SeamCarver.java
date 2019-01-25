5b8c5a02a030a890ff9ef066083ded4efc010a06
s-wonget <s-wonget@51L104-14.it.bsd405.org>
2019-01-25 08:53:20 -0800

    Imported and implemented picture width and height

--- SeamCarving/src/SeamCarver.java
+++ SeamCarving/src/SeamCarver.java
@@ -1,46 +1,40 @@
-public class SeamCarver
-{
-	public SeamCarver(SmC_Picture pictureP)
-	{
+public class SeamCarver{
+	private SmC_Picture p;
+	
+	public SeamCarver(SmC_Picture pictureP){
+		p = pictureP;
 	}
 
-	public SmC_Picture picture()
-	{
+	public SmC_Picture picture(){
+		return p;
+	}
+
+	public int width(){
+		return p.width();
+	}
+
+	public int height(){
+		return p.height();
+	}
+
+	public double energy(int x, int y){
 		throw new UnsupportedOperationException();
 	}
 
-	public int width()
-	{
+	public int[] findHorizontalSeam(){
 		throw new UnsupportedOperationException();
 	}
 
-	public int height()
-	{
+	public int[] findVerticalSeam(){
 		throw new UnsupportedOperationException();
 	}
 
-	public double energy(int x, int y)
-	{
+	public void removeHorizontalSeam(int[] a){
 		throw new UnsupportedOperationException();
 	}
 
-	public int[] findHorizontalSeam()
-	{
+	public void removeVerticalSeam(int[] a){
 		throw new UnsupportedOperationException();
 	}
-
-	public int[] findVerticalSeam()
-	{
-		throw new UnsupportedOperationException();
-	}
-
-	public void removeHorizontalSeam(int[] a)
-	{
-		throw new UnsupportedOperationException();
-	}
-
-	public void removeVerticalSeam(int[] a)
-	{
-		throw new UnsupportedOperationException();
-	}
+	
 }
